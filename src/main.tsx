a384c2066064dc8aada366c149ce419edce54c80
mac <mac@Rigor08.local>
2024-10-05 11:18:22 +0100

    chore: update and install new dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ExpenseForm from './components/Form/ExpenseForm';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ExpenseForm />
   </StrictMode>,
 )
